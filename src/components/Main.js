605b63a567c01b3684aa39eb29aca38e91b28938
pupil <pupil@ivkhk.ee>
2024-04-10 13:35:36 +0300

    update

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,11 +6,13 @@ function Main() {
             <aside className='search-aside'>
                 <form>
                     <select>
+                        <option>Марка</option>
                         <option>BMW</option>
                         <option>Volkswagen</option>
                         <option>Audi</option>
                     </select>
                     <select>
+                        <option>Модель</option>
                         <option>530</option>
                         <option>Golf</option>
                         <option>R8</option>
@@ -18,55 +20,57 @@ function Main() {
                     <div className='range-input'>
                         <label>Год</label>
                         <div>
-                            <input></input>
+                            <input type='number'></input>
                             -
-                            <input></input>
+                            <input type='number'></input>
                         </div>
                     </div>
                     <div className='range-input'>
                         <label>Цена</label>
                         <div>
-                            <input></input>
+                            <input type='number'></input>
                             -
-                            <input></input>
+                            <input type='number'></input>
                         </div>
                     </div>
                     <div className='range-input'>
                         <label>Мощность (кВ)</label>
                         <div>
-                            <input></input>
+                            <input type='number'></input>
                             -
-                            <input></input>
+                            <input type='number'></input>
                         </div>
                     </div>
                     <div className='range-input'>
                         <label>Пробег (км)</label>
                         <div>
-                            <input></input>
+                            <input type='number'></input>
                             -
-                            <input></input>
+                            <input type='number'></input>
                         </div>
                     </div>
-                    <select className='sosi'>
+                    <select className='fuel-select'>
                         <option>Топливо</option>
                         <option>Дизель</option>
                         <option>Бензин</option>
                         <option>Электричество</option>
                     </select>
                     <select>
-                        <option>BMW</option>
-                        <option>Volkswagen</option>
-                        <option>Audi</option>
+                        <option>КПП</option>
+                        <option>Автомат</option>
+                        <option>Механическая коробка передач</option>
                     </select>
                     <select>
-                        <option>BMW</option>
-                        <option>Volkswagen</option>
-                        <option>Audi</option>
+                        <option>Ведущие колеса</option>
+                        <option>Задние ведущие</option>
+                        <option>Передние ведущие</option>
+                        <option>4x4</option>
                     </select>
                     <select>
-                        <option>BMW</option>
-                        <option>Volkswagen</option>
-                        <option>Audi</option>
+                        <option>Местоположение</option>
+                        <option>Кохтла-Ярве</option>
+                        <option>Таллинн</option>
+                        <option>Тарту</option>
                     </select>
                     <button type='submit'>Искать</button>
                 </form>
@@ -74,7 +78,7 @@ function Main() {
             <div>
                 <div className='top-panel'>
                     <p>Найдено:</p>
-                    <select>
+                    <select className='sort-select'>
                         <option>Сортировка</option>
                         <option>Volkswagen</option>
                         <option>Audi</option>
