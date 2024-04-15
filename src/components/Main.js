d8b711e2a02780abec9bdb40c77633fe76e26d66
pupil <pupil@ivkhk.ee>
2024-04-15 11:14:27 +0300

    update

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import car from "../assets/car.png"
 
 function Main() {
+    const [select, setSelect] = useState("#00000050");
+    const handler = (e) => {
+        console.log(e);
+        if (e.target.value == 0) {
+            e.target.style.color = "#00000050"
+            // setSelect("#00000050")
+        } else {
+            e.target.style.color = "#000"
+            // setSelect("#000")
+        }
+    }
+
     return (
         <div className='main-div'>
             <aside className='search-aside'>
                 <form>
-                    <select>
-                        <option>Марка</option>
-                        <option>BMW</option>
-                        <option>Volkswagen</option>
-                        <option>Audi</option>
+                    <select onChange={(e) => { handler(e) }} style={{ color: select}}>
+                        <option value={0}>Марка</option>
+                        <option value={1}>BMW</option>
+                        <option value={2}>Volkswagen</option>
+                        <option value={3}>Audi</option>
                     </select>
-                    <select>
+                    <select onChange={(e) => { handler(e) }} style={{ color: select}}>
                         <option>Модель</option>
                         <option>530</option>
                         <option>Golf</option>
@@ -20,7 +33,7 @@ function Main() {
                     <div className='range-input'>
                         <label>Год</label>
                         <div>
-                            <input type='number'></input>
+                            <input type='number' min={0}></input>
                             -
                             <input type='number'></input>
                         </div>
@@ -28,7 +41,7 @@ function Main() {
                     <div className='range-input'>
                         <label>Цена</label>
                         <div>
-                            <input type='number'></input>
+                            <input type='number' min={0}></input>
                             -
                             <input type='number'></input>
                         </div>
@@ -36,7 +49,7 @@ function Main() {
                     <div className='range-input'>
                         <label>Мощность (кВ)</label>
                         <div>
-                            <input type='number'></input>
+                            <input type='number' min={0}></input>
                             -
                             <input type='number'></input>
                         </div>
@@ -44,29 +57,29 @@ function Main() {
                     <div className='range-input'>
                         <label>Пробег (км)</label>
                         <div>
-                            <input type='number'></input>
+                            <input type='number' min={0}></input>
                             -
                             <input type='number'></input>
                         </div>
                     </div>
-                    <select className='fuel-select'>
+                    <select onChange={(e) => { handler(e) }} style={{ color: select}} className='fuel-select'>
                         <option>Топливо</option>
                         <option>Дизель</option>
                         <option>Бензин</option>
                         <option>Электричество</option>
                     </select>
-                    <select>
+                    <select onChange={(e) => { handler(e) }} style={{ color: select}}>
                         <option>КПП</option>
                         <option>Автомат</option>
                         <option>Механическая коробка передач</option>
                     </select>
-                    <select>
+                    <select onChange={(e) => { handler(e) }} style={{ color: select}}>
                         <option>Ведущие колеса</option>
                         <option>Задние ведущие</option>
                         <option>Передние ведущие</option>
                         <option>4x4</option>
                     </select>
-                    <select>
+                    <select onChange={(e) => { handler(e) }} style={{ color: select}}>
                         <option>Местоположение</option>
                         <option>Кохтла-Ярве</option>
                         <option>Таллинн</option>
@@ -83,6 +96,50 @@ function Main() {
                         <option>Volkswagen</option>
                         <option>Audi</option>
                     </select>
+                </div>
+                <div className='cardList'>
+                    <div className='card'>
+                        <span className='newOffer'>
+                            Новое предложение
+                        </span>
+                        <img src={car} />
+                        <div>
+                            <div className='mainInfo'>
+                                <p>Skoda Superb</p>
+                                <p>13500e</p>
+                                <p>2017</p>
+                            </div>
+                            <div className='badges'>
+                                <span>188500km</span>
+                                <span>Diesel</span>
+                                <span>Automat</span>
+                                <span>Avant</span>
+                                <span>Foreground</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='cardList'>
+                    <div className='card'>
+                        <span className='newOffer'>
+                            Новое предложение
+                        </span>
+                        <img src={car} />
+                        <div>
+                            <div className='mainInfo'>
+                                <p>Skoda Superb</p>
+                                <p>13500e</p>
+                                <p>2017</p>
+                            </div>
+                            <div className='badges'>
+                                <span>188500km</span>
+                                <span>Diesel</span>
+                                <span>Automat</span>
+                                <span>Avant</span>
+                                <span>Foreground</span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
