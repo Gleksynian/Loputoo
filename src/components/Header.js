c8987f8889a761e64f9d69c4c4cf17d290aa7cef
pupil <pupil@ivkhk.ee>
2024-04-22 11:12:43 +0300

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from 'react-modal';
 import '../App.css'
 
 function Header() {
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+
+    const openModal = () => {
+        setModalIsOpen(true);
+    };
+
+    const closeModal = () => {
+        setModalIsOpen(false);
+    };
+    const modalContent = (
+        <div>
+            <div className='loginRegisterWindows'>
+                <div className='loginDivWindow'>
+                    <h1>Вход</h1>
+                </div>
+                <div className='registerDivWindow'>
+                    <h1>Регистрация</h1>
+                </div>
+                <form>
+                    <input placeholder='* E-Mail'></input>
+                    <input placeholder='* Пароль'></input>
+                </form>
+                <button>Войти</button>
+            </div>
+        </div>
+    );
     return (
         <header>
             <h1>AutoKaup24</h1>
-           <ul>
-            <li>Создать объявление</li>
-            <li>Избранное</li>
-            
-           </ul>
-           <ul className='login-register'>
-            <li>Войти</li>
-            <li>Зарегистрироваться</li>
-           </ul>
+            <ul>
+                <li>Создать объявление</li>
+                <li>Избранное</li>
+
+            </ul>
+            <ul className='login-register'>
+                <li>
+                    <button onClick={openModal}>Войти</button>
+                </li>
+                <Modal isOpen={modalIsOpen} onRequestClose={closeModal}>
+                    {modalContent}
+                </Modal>
+                <li>Зарегистрироваться</li>
+            </ul>
         </header>
     )
 }
