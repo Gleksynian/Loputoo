f2729802c951d3ccb48357c46eb1a14961767d8f
pupil <pupil@ivkhk.ee>
2024-04-29 15:14:18 +0300

    update

--- src/components/PlaceAnAd.js
+++ src/components/PlaceAnAd.js
@@ -20,76 +20,82 @@ function PlaceAnAd() {
                 <Header />
                 <div className='placeAdDiv'>
                     <form className='adForm'>
-                            <select onChange={(e) => { handler(e) }} style={{ color: select }}>
-                                <option hidden value={0}>Марка</option>
-                                <option value={1}>BMW</option>
-                                <option value={2}>Volkswagen</option>
-                                <option value={3}>Audi</option>
-                            </select>
-                            <select onChange={(e) => { handler(e) }} style={{ color: select }}>
-                                <option hidden>Модель</option>
-                                <option>530</option>
-                                <option>Golf</option>
-                                <option>R8</option>
-                            </select>
-                            <div className='range-input'>
-                                <label>Год</label>
-                                <div>
-                                    <input type='number' min={0}></input>
+                        <div className='divAd'>
+                            <div className='formDivAd'>
+                                <select onChange={(e) => { handler(e) }} style={{ color: select }}>
+                                    <option hidden value={0}>Марка</option>
+                                    <option value={1}>BMW</option>
+                                    <option value={2}>Volkswagen</option>
+                                    <option value={3}>Audi</option>
+                                </select>
+                                <select onChange={(e) => { handler(e) }} style={{ color: select }}>
+                                    <option hidden>Модель</option>
+                                    <option>530</option>
+                                    <option>Golf</option>
+                                    <option>R8</option>
+                                </select>
+                                <div className='range-input'>
+                                    <label>Год</label>
+                                    <div>
+                                        <input type='number' min={0}></input>
+                                    </div>
+                                </div>
+                                <div className='range-input'>
+                                    <label>Цена</label>
+                                    <div>
+                                        <input type='number' min={0}></input>
+                                    </div>
+                                </div>
+                                <div className='range-input'>
+                                    <label className='powerLabel'>Мощность (кВ)</label>
+                                    <div>
+                                        <input type='number' min={0}></input>
+                                    </div>
+                                </div>
+                                <div className='range-input'>
+                                    <label>Пробег (км)</label>
+                                    <div>
+                                        <input type='number' min={0}></input>
+                                    </div>
+                                </div>
+                                <select onChange={(e) => { handler(e) }} style={{ color: select }} className='fuel-select'>
+                                    <option hidden>Топливо</option>
+                                    <option>Дизель</option>
+                                    <option>Бензин</option>
+                                    <option>Электричество</option>
+                                </select>
+                                <select onChange={(e) => { handler(e) }} style={{ color: select }}>
+                                    <option hidden>КПП</option>
+                                    <option>Автомат</option>
+                                    <option>Механическая коробка передач</option>
+                                </select>
+                                <select onChange={(e) => { handler(e) }} style={{ color: select }}>
+                                    <option hidden>Ведущие колеса</option>
+                                    <option>Задние ведущие</option>
+                                    <option>Передние ведущие</option>
+                                    <option>4x4</option>
+                                </select>
+                                <select onChange={(e) => { handler(e) }} style={{ color: select }}>
+                                    <option hidden>Местоположение</option>
+                                    <option>Кохтла-Ярве</option>
+                                    <option>Таллинн</option>
+                                    <option>Тарту</option>
+                                </select>
+                            </div>
+                            <div>
+                                <div className='additionalInfoAd'>
+                                    <label>Дополнительная информация</label>
+                                    <textarea className='additionalInputAd' type='text'></textarea>
+                                </div>
+                                <div className='mainPhotoAd'>
+                                    <label>Главная фотография</label>
+                                    <label className='adPhotoLabel'>
+                                        <img className='image-photo' src={photoAdForm} />
+                                        <input type='file' hidden/>
+                                    </label>
                                 </div>
                             </div>
-                            <div className='range-input'>
-                                <label>Цена</label>
-                                <div>
-                                    <input type='number' min={0}></input>
-                                </div>
-                            </div>
-                            <div className='range-input'>
-                                <label className='powerLabel'>Мощность (кВ)</label>
-                                <div>
-                                    <input type='number' min={0}></input>
-                                </div>
-                            </div>
-                            <div className='range-input'>
-                                <label>Пробег (км)</label>
-                                <div>
-                                    <input type='number' min={0}></input>
-                                </div>
-                            </div>
-                            <select onChange={(e) => { handler(e) }} style={{ color: select }} className='fuel-select'>
-                                <option hidden>Топливо</option>
-                                <option>Дизель</option>
-                                <option>Бензин</option>
-                                <option>Электричество</option>
-                            </select>
-                            <select onChange={(e) => { handler(e) }} style={{ color: select }}>
-                                <option hidden>КПП</option>
-                                <option>Автомат</option>
-                                <option>Механическая коробка передач</option>
-                            </select>
-                            <select onChange={(e) => { handler(e) }} style={{ color: select }}>
-                                <option hidden>Ведущие колеса</option>
-                                <option>Задние ведущие</option>
-                                <option>Передние ведущие</option>
-                                <option>4x4</option>
-                            </select>
-                            <select onChange={(e) => { handler(e) }} style={{ color: select }}>
-                                <option hidden>Местоположение</option>
-                                <option>Кохтла-Ярве</option>
-                                <option>Таллинн</option>
-                                <option>Тарту</option>
-                            </select>
-                            <div className='additionalInfoAd'>
-                                <label>Дополнительная информация</label>
-                                <input className='additionalInputAd' type='text'></input>
-                            </div>
-                            <div className='mainPhotoAd'>
-                                <label>Главная фотография</label>
-                                <label className='adPhotoLabel'>
-                                    <img className='image-photo' src={photoAdForm} />
-                                    <input type='file' hidden></input>
-                                </label>
-                            </div>
+                        </div>
                         <button className='btn-submit-ad' type='submit'>Подтвердить</button>
                     </form>
                 </div>
